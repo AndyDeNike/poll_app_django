58ee328237df7942deaeb9b6ebfaf3aff2c94e2c
Andrew DeNike <denikeandrew@gmail.com>
2018-06-14 19:58:12 -0700

    Added human readable method for classes/was_published_recently method for Question

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime 
+
 from django.db import models
+from django.utils import timezone
+
+
+
 
 # Create your models here.
 # Each model has a number of class variables that represent a database
@@ -25,9 +31,19 @@ class Question(models.Model):
 	# if arugment not given, Field instance name is default 
 	pub_date = models.DateTimeField('date published')
 
+	#human readable: 
+	def __str__(self):
+		return self.question.text 
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	# optional argument default is set to 0 for votes 
 	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.choice_text
 
