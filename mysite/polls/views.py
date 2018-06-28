8409d9168e1b35ae16f6c95105b1a68d52b0e161
Andrew DeNike <denikeandrew@gmail.com>
2018-06-28 15:04:37 -0700

    Updated inex method render/added to detail view method

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import Http404
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
@@ -12,11 +14,28 @@ from .models import Question
 def index(request):
 	#return HttpResponse("Hello, world.  You're at the polls index.")
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ', '.join([q.question_text for q in latest_question_list])
-	return HttpResponse(output)
+	#output = ', '.join([q.question_text for q in latest_question_list])
+	#loads template and passes context 
+	#template = loader.get_template('polls/index.html')
+	#context creates a dictionary which connects template variables to 
+	#views in python objects 
+	context = {'latest_question_list': latest_question_list}
+	#return HttpResponse(template.render(context, request))
+	#^ rewritten: 
+	#render takes the request object as 1st argument, template name as
+	#2nd argument, and a dictionary with template variables as optional 
+	#3rd argument
+	#returns HttpResponse object of template rendered w/ given context
+	return render(request, 'polls/index.html', context)
 	
 def detail(request, question_id): 
-	return HttpResponse("You're looking at question %s." % question_id)
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question': question})
+
+	#return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
 	response = "You're looking at the results of quesiton %s."
