f1ce1915784d7791781bee148b8119f81d4d7670
Andrew DeNike <denikeandrew@gmail.com>
2018-08-04 22:28:43 -0700

    Replaced urlConf to setup generic code

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views 
 
 #path()argument contains 2 required parameters and 2 optional: 
-#1st 'route': a string that contains a URL pattern that when requested,
+#1st 'route': a string	 that contains a URL pattern that when requested,
 #is searched for in the urlpatterns list
 #2nd 'view': when mathc route match is found, Django calls specified 
 #view function, ex. views.whatever 
@@ -23,5 +23,6 @@ urlpatterns = [
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
 
