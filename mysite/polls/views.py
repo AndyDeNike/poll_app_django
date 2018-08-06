254491b0514b455a3eb2f366ee561004db80950d
Andrew DeNike <denikeandrew@gmail.com>
2018-08-06 16:37:34 -0700

    Implemented generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,7 +13,7 @@ from django.views import generic
 from .models import Choice, Question
 
 # Create your views here.
-'''
+
 class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
 	context_object_name = 'latest_question_list'
@@ -31,11 +31,26 @@ class ResultView(generic.DetailView):
 	template_name = 'polls/results.html'
 
 def vote(request, question_id):
-	pass #same as above, no changes needed. 
-	#added this comment for git test
-'''
-		
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form.
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+		})
+	else: 
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data.  This prevents data from being used twice if a 
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+	#return HttpResponse("You're voting on question %s." % question_id)
 
+		
+'''
 def index(request):
 	#return HttpResponse("Hello, world.  You're at the polls index.")
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -88,3 +103,4 @@ def vote(request, question_id):
 		# user hits the Back button.
 		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 	#return HttpResponse("You're voting on question %s." % question_id)
+'''
