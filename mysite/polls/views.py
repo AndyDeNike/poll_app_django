1df3dd237993a49c0a13684388c8f88ee320903b
Andrew DeNike <denikeandrew@gmail.com>
2018-08-10 16:27:01 -0700

    Fixed error w/ class ResultsView

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -26,7 +26,7 @@ class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
 
-class ResultView(generic.DetailView):
+class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
 
