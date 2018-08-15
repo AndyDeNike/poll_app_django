e762ed255b59f1e97c8d785d28d54d10d5381c40
Andrew DeNike <denikeandrew@gmail.com>
2018-08-14 23:14:33 -0700

    Created test def create_question()

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -38,3 +38,13 @@ class QuestionModelTests(TestCase):
 		time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
 		recent_question = Question(pub_date=time)
 		self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
