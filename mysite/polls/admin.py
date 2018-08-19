90828bbc02ede6267ca8c83d1997eeff8cca279c
Andrew DeNike <denikeandrew@gmail.com>
2018-08-18 23:02:43 -0700

    Adjust admin.py with class for custom control

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -7,4 +7,12 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+from django.contrib import admin 
+
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
