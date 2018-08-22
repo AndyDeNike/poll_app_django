afda491ffb164cae26a5c8010f692ab267d5b4a0
Andrew DeNike <denikeandrew@gmail.com>
2018-08-21 22:49:07 -0700

    Added list_filter to QuestionAdmin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -23,6 +23,7 @@ class QuestionAdmin(admin.ModelAdmin):
 	]
 	inlines = [ChoiceInline]
 	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
 	#fields = ['pub_date', 'question_text']
 
 admin.site.register(Question, QuestionAdmin)
